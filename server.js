783fba4c475ad58154ef3efa8cdfa44faaa5eea3
Ahmed Morsy <morsyahmed402@gamil.com>
2023-07-22 20:54:34 +0300

    new

--- server.js
+++ server.js
@@ -3,12 +3,34 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const rappers = {
+  "21 savage": {
+    age: 31,
+    birthName: "Sheyaa Bin Abraham-Joseph",
+    birthLocation: "London, England",
+  },
+  "chance the rapper": {
+    age: 30,
+    birthName: "Chancelor Bennett",
+    birthLocation: "Chicago, Illinois",
+  },
+  morsy: {
+    age: 23,
+    birthName: "Ahmed Morsy",
+    birthLocation: "Menofia Qwesna",
+  },
+};
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/rapper", (req, res) => {
-  res.send("Mother Fucker");
+app.get("/rappers/:rapperName", (req, res) => {
+  console.log(req.params.rapperName);
+  const rapperName = req.params.rapperName.toLowerCase();
+
+  if (rappers[rapperName]) return res.json(rappers[rapperName]);
+  else return res.json({ msg: "Sorry don't know that guy!!!" });
 });
 
 app.listen(port, () => console.log(`Server is Running on Port ${port}...`));
