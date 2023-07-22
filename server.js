a7c69497e0153fc75f12e2fa11d67ac8de64f2b9
Ahmed Morsy <morsyahmed402@gamil.com>
2023-07-22 17:21:48 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => console.log(`Server is Running on Port ${port}...`));
