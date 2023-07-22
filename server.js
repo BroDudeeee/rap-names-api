bc45ce95c7f5a2dbe89a0eade0e1b5149234d4fc
Ahmed Morsy <morsyahmed402@gamil.com>
2023-07-22 17:26:22 +0300

    test

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/rapper", (req, res) => {
+  res.send("Mother Fucker");
+});
+
 app.listen(port, () => console.log(`Server is Running on Port ${port}...`));
